062a35bfbdcf688aa488de8b4fe2cfc7655742d4
Ousmane Kanoute <MrGzima@outlook.fr>
2025-04-30 20:30:50 +0200

    Composant animation logo, rotation et déplacement du logo ClairObscur lors du scroll

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { div } from "three/tsl";
+
+export const HeroSection = () => {
+  return <div className="h-screen bg-red-800"></div>;
+};
+
+export default HeroSection;
