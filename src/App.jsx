fb62f3ea5edcf656643b8f9a5de447348c6aa151
Ousmane Kanoute <MrGzima@outlook.fr>
2025-05-16 16:35:42 +0200

    Section projets progression

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Model } from "./components/Model";
 import "./App.css";
@@ -8,9 +8,32 @@ import { RotatingLogoScene } from "./components/RotatingLogoScene";
 import { Pitch } from "./components/Pitch";
 import { Header } from "./components/Header";
 import PitchIntroSection from "./components/PitchIntroSection";
-import Gallery from "./components/Gallery";
+import { Gallery } from "./components/Gallery";
+import { Project1 } from "./components/Project1";
+import { Project2 } from "./components/Project2";
+import { ScrollControls } from "@react-three/drei";
+
+import Lenis from "lenis";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis({
+      // Valeur entre 0 et 1
+      // Valeur par défaut : 0,1
+      // Plus la valeur est faible, plus le scroll sera fluide
+      lerp: 0.05,
+      // Valeur par défaut : 1
+      // Plus la valeur est haute, plus le défilement sera rapide
+      wheelMultiplier: 1,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
   return (
     <>
       <Header />
@@ -18,7 +41,8 @@ function App() {
       <Pitch />
       <RotatingLogoScene />
       <Gallery />
-      <HeroSection />
+      <Project1 />
+      <Project2 />
     </>
   );
 }
