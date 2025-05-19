5b23164dc55cdc2447d7231094f20b7be54bba95
Ousmane Kanoute <MrGzima@outlook.fr>
2025-05-19 13:32:47 +0200

    Reparation effect parallax au redimensionnement

--- src/components/ProjectSection2.jsx
+++ src/components/ProjectSection2.jsx
@@ -73,7 +73,7 @@ export const ProjectSection2 = () => {
         ScrollTrigger.getAll().forEach((trigger) => trigger.kill());
       };
     }
-  }, []);
+  }, [width, height]);
 
   return (
     <div className="relative content h-screen flex flex-col bg-white overflow-hidden">
