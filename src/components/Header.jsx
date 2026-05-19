9d8bb8de0116820432d2d1ff9c05e41f1842cfa2
Ousmane Kanoute <MrGzima@outlook.fr>
2026-05-19 14:11:29 +0200

    refonte du header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 import React, { useEffect, useState } from "react";
 import item from "/logo/itemBlanc.svg";
 import logo from "/logo/logo.svg";
+import { useIsMobile } from "../utils/useIsMobile";
 import { Link, useNavigate, useLocation } from "react-router-dom";
+import { useWindowSize } from "../utils/useWindowSize";
 
 export const Header = () => {
   const navigate = useNavigate();
   const location = useLocation();
+  const isMobile = useIsMobile();
+  const { width, height } = useWindowSize();
   const [open, setOpen] = useState(false);
 
   // fermeture avec Échap
@@ -65,14 +69,53 @@ export const Header = () => {
         </div>
 
         <div className="font-fujiwara-black-italic col-start-3 flex justify-end text-lg sm:text-xl lg:text-3xl w-auto z-[100]">
-          <button
-            className="cursor-pointer"
-            onClick={() => setOpen(true)}
-            aria-expanded={open}
-            aria-controls="site-menu"
-          >
-            [MENU]
-          </button>
+          {isMobile || width < 1024 ? (
+            <button
+              className="cursor-pointer"
+              onClick={() => setOpen(true)}
+              aria-expanded={open}
+              aria-controls="site-menu"
+            >
+              MENU
+            </button>
+          ) : (
+            <div className="flex gap-4 text-xl bg-white/10 backdrop-blur-2xl px-5 py-1 rounded-2xl">
+              {/* Galerie */}
+              <Link className="self-end" to="/galerie">
+                <button
+                  className="self-end cursor-pointer"
+                  onClick={() => {
+                    setOpen(false);
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                  }}
+                >
+                  Galerie
+                </button>
+              </Link>
+              <Link className="self-end" to="/credits">
+                <button
+                  className="self-end cursor-pointer"
+                  onClick={() => {
+                    setOpen(false);
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                  }}
+                >
+                  Crédits
+                </button>
+              </Link>
+              <Link className="self-end" to="/contact">
+                <button
+                  className="self-end cursor-pointer"
+                  onClick={() => {
+                    setOpen(false);
+                    window.scrollTo({ top: 0, behavior: "smooth" });
+                  }}
+                >
+                  Contact
+                </button>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
 
@@ -84,26 +127,10 @@ export const Header = () => {
         >
           <div className="w-full flex justify-end border-b border-black/10 px-5 py-10 sm:px-10 sm:py-10 lg:px-20 lg:py-10 text-5xl lg:text-5xl">
             <button className="cursor-pointer" onClick={() => setOpen(false)}>
-              [X]
+              X
             </button>
           </div>
           <div className="flex flex-col w-full px-20 py-12 gap-2">
-            {/* Pitch */}
-            <button
-              className="self-end cursor-pointer"
-              onClick={() => {
-                setOpen(false);
-                if (location.pathname === "/") {
-                  const el = document.getElementById("pitchSection");
-                  if (el) el.scrollIntoView({ behavior: "smooth" });
-                } else {
-                  navigate("/", { state: { section: "pitchSection" } });
-                }
-              }}
-            >
-              Pitch
-            </button>
-
             {/* Galerie */}
             <Link className="self-end" to="/galerie">
               <button
